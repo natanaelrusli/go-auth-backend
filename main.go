cc23d7d6692bd2ebddd551497a34a03d55787863
Nata Nael <nata.nael@shopee.com>
2022-10-07 20:01:05 +0700

    add: initRouter in main.go

--- main.go
+++ main.go
@@ -1,8 +1,33 @@
 package main
 
-import "go-auth-backend/database"
+import (
+	"go-auth-backend/controllers"
+	"go-auth-backend/database"
+	"go-auth-backend/middlewares"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	// init database
 	database.Connect("root:password@tcp(localhost:3306)/jwt_demo?parseTime=true")
 	database.Migrate()
+
+	// init router
+	router := initRouter()
+	router.Run(":8080")
+}
+
+func initRouter() *gin.Engine {
+	router := gin.Default()
+	api := router.Group("/api")
+	{
+		api.POST("/token", controllers.GenerateToken)
+		api.POST("user/register", controllers.RegisterUser)
+		secured := api.Group("/secured").Use(middlewares.Auth())
+		{
+			secured.GET("/ping", controllers.Ping)
+		}
+	}
+	return router
 }
