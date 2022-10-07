dc12b965a9223a3452abb8f1939634cd4c45bd2a
Nata Nael <nata.nael@shopee.com>
2022-10-07 19:13:32 +0700

    add: token controller

---
+++ controllers/token_controller.go
@@ -0,0 +1,47 @@
+package controllers
+
+import (
+	"go-auth-backend/auth"
+	"go-auth-backend/database"
+	"go-auth-backend/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type TokenRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func GenerateToken(context *gin.Context) {
+	var request TokenRequest
+	var user models.User
+
+	if err := context.ShouldBindJSON(&request); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		context.Abort()
+		return
+	}
+
+	// check if email exists AND password is correct
+	record := database.Instance.Where("email = ?", request.Email).First(&user)
+	if record.Error != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": record.Error.Error()})
+	}
+
+	credentialError := user.CheckPassword(request.Password)
+	if credentialError != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"error": "invalid credentials"})
+		context.Abort()
+		return
+	}
+
+	tokenString, err := auth.GenerateJWT(user.Email, user.Username)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		context.Abort()
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"token": tokenString})
+}
